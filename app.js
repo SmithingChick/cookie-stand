407d1d7e26f6bca0b0d8f3f0edbee3949149c4f5
SmithingChick <sharon.smith.metals@gmail.com>
2016-09-16 17:12:49 -0700

    old changes I apparently forgot to push up to github

--- app.js
+++ app.js
@@ -62,23 +62,7 @@ Store.prototype.render = function () { // creating table row
     var bodyContent = document.createElement('td');
     bodyContent.textContent = hourTotal; //store avg hour sales
     //is hours array set up properly?
-    //from Day 11 Code Review
-  //  for(var p= 0 ; p stores.length; p++) {
-  //    var row= stores[p];render();
-  //    tableBody.appendChild(row);
-  //  } partially copied from code review
-  //  for (var storeC = 0; storeC < stores.length; storeC++){
-  //    hourTotal +=stores[storeC].hourly[hours]; hours = 1
-  //    totalData.textContent = hourTotal[hours];
-  //  }
-  // var hourTotal
-  //var totalHead.textContent = 'Total';
-  // for (var hours = 1; hours <time.length; storeC++){
-  //    var td = document.createElement('td');
-  //    hourTotal = 0;
-  //    console.log(time[hours]);
-  //  for var storeC
-  //  }
+ 
     tableRow.appendChild(bodyContent);
     console.log('begin render');
   }
@@ -121,20 +105,3 @@ sCenter.render();
 capital.render();
 alki.render();
 
-
-
-// var main = document.getElementById('sales_data');
-// main.appendChild(salesTable);
-//
-//
-//Store.prototype.render = function () { // creating table row
-//  var bodyContent = [Math, math, math, math, math, math, math];
-//  var salesBody = document.createElement('tbody');
-//  var bodyRow = document.createElement('tr');
-  // var mondayData = document.createElement('td');
-  // var tuesdayData = document.createElement('td');
-//  rowHeader.textContent = name;
-//  var main = document.getElementById('store_info');
-//  return document.getElementById('store_info');
-//  console.log('render prototype');
-//};
---
+++ odds-ends.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+   //from Day 11 Code Review
+  //  for(var p= 0 ; p stores.length; p++) {
+  //    var row= stores[p];render();
+  //    tableBody.appendChild(row);
+  //  } partially copied from code review
+  //  for (var storeC = 0; storeC < stores.length; storeC++){
+  //    hourTotal +=stores[storeC].hourly[hours]; hours = 1
+  //    totalData.textContent = hourTotal[hours];
+  //  }
+  // var hourTotal
+  //var totalHead.textContent = 'Total';
+  // for (var hours = 1; hours <time.length; storeC++){
+  //    var td = document.createElement('td');
+  //    hourTotal = 0;
+  //    console.log(time[hours]);
+  //  for var storeC
+  //  }
+	
+	
+
+// var main = document.getElementById('sales_data');
+// main.appendChild(salesTable);
+//
+//
+//Store.prototype.render = function () { // creating table row
+//  var bodyContent = [Math, math, math, math, math, math, math];
+//  var salesBody = document.createElement('tbody');
+//  var bodyRow = document.createElement('tr');
+  // var mondayData = document.createElement('td');
+  // var tuesdayData = document.createElement('td');
+//  rowHeader.textContent = name;
+//  var main = document.getElementById('store_info');
+//  return document.getElementById('store_info');
+//  console.log('render prototype');
+//};
