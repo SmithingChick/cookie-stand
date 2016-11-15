018255193bb3e74603aea635a22708f39b523507
SmithingChick <sharon.smith.metals@gmail.com>
2016-11-15 09:15:59 -0800

    trying to get random number list to work...again

--- app.js
+++ app.js
@@ -1,107 +1,90 @@
 'use strict';
 
-//table
-console.log('begin table');
-function tableCreate() {
-  var main = document.getElementById('sales_data');
-  var tbl = document.createElement('table');
-  var thead = document.createElement('thead');
-  var tbody = document.createElement('tbody');
-  var headerContent = ['', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
-  //table head
-  var titleRow = document.createElement('tr');
-  for(var i = 0; i < headerContent.length; i++) {
-    var th = document.createElement('th');
-    th.textContent = headerContent[i];
-    titleRow.appendChild(th);
-    console.log('table header');
+//Generate numer of cookies per hour per store
+
+//Generate list of total cookies per hour
+var cookieStore = {
+  name: 'Pike Place',
+  storeHours: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '],
+  whatHours: function() {
+    console.log('I am open certain hours: ' + this.storeHours);
+  },
+  listHours: function() {
+    var contentArea = document.getElementById('sales_data');
+    var ul = document.createElement('ul');
+    var li;
+
+    for (var i = 0; i < this.storeHours.length; i++) {
+      li = document.createElement('li');
+
+      li.textContent = this.storeHours[i];
+      ul.appendChild(li);
+    }
+    contentArea.appendChild(ul);
   }
-  //table body
-  var tableRow = document.createElement('tr');
-  var rowHeader = document.createElement('th');
-  rowHeader.textContent = name;
-  tableRow.appendChild(rowHeader);
-  for (i = 0; i < headerContent.length - 1; i++) {
-    var bodyContent = document.createElement('td');
-    bodyContent.textContent = this.totalCookiesPerHour;
-    tableRow.appendChild(bodyContent);
-    console.log('begin render');
-  }
-  //end of store render
-  tbl.setAttribute('id', 'data_table');
-  tbody.appendChild(tableRow);
-  thead.appendChild(titleRow);
-  tbl.appendChild(tbody);
-  tbl.appendChild(thead);
-  main.appendChild(tbl);
-  console.log('table end');
-}
-console.log('table up?');
-tableCreate();
+};
+cookieStore.whatHours();
+cookieStore.listHours();
 
 //store info - name, average cookies, minimum daily customers, maximum daily customers
-function Store(name, avg, min, max) {
-  this.name = name;
-  this.avgCookies = avg;
-  this.minCust = min;
-  this.maxCust = max;
-  console.log(this.name, this.avgCookies, this.minCust, this.maxCust);
-  this.totalCookiesPerDay = 0;
-  this.totalCookiesPerHour = [];
-  console.log(this.totalCookiesPerDay, this.totalCookiesPerHour);
-}
-
-Store.prototype.render = function () { // creating table row
-  var table = document.getElementById('data_table');
-  console.log('table-render', table);
-  var tableRow = document.createElement('tr');
-  console.log('render');
-  var rowHeader = document.createElement('th');
-  tableRow.appendChild(rowHeader);
-  for (var i = 0; i < 16; i++) {
-    var bodyContent = document.createElement('td');
-    bodyContent.textContent = hourTotal; //store avg hour sales
-    //is hours array set up properly?
- 
-    tableRow.appendChild(bodyContent);
-    console.log('begin render');
-  }
-  table.appendChild(tableRow);
-};
-
-//generate number of hourly customers per store
-Store.prototype.generateRandom = function () {
-  return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  console.log('math');
-};
-//generate number of cookies per hour
-Store.prototype.cookiesPerHour = function () {
-  return Math.floor(this.generateRandom() * this.avgCookies);
-  this.generateRandom = hourTotal ;
-};
-//generate number of total cooies per day
-Store.prototype.cookiesPerDay = function () {
-//    var cookiesDay;
-  for (var i = 6; i < 20; i++) {
-    var cookiesHere = this.cookiesPerHour();
-    this.hourCookies.push(cookiesHere);
-    this.totalCookies += cookiesHere;
+var cookiesHour = {
+  Store: function(name, avg, min, max) {
+    this.name = name;
+    this.avgCookies = avg;
+    this.minCust = min;
+    this.maxCust = max;
+    console.log(this.name, this.avgCookies, this.minCust, this.maxCust);
   }
 };
+Store.cookiesHour();
 
+// var getHourSales = {
+//   hourTotal: function(min, max) {
+//   this.hourSalesMax = Math.ceil(min);
+//   this.hourSalesMax = Math.floor(max);
+//   return Math.floor(Math.random() * (this.hourSalesMax - this.hourSalesMin) + this.hourSalesMax);
+//   console.log(this.hourSalesMin, this.hourSalesMax);
+// }
+// hourTotal.getHourSales();
 
 var pikePlace = new Store('Pike Place', 6.3, 23, 65);
 var seaTac = new Store('SeaTac Airport', 1.2, 3 , 24);
-var sCenter = new Store('Seattle Center', 3.7, 11, 28);
-var capital = new Store('Capital Hill', 2.3, 20, 38);
-var alki = new Store('Alki', 4.6, 2, 16);
-
-console.log(pikePlace, seaTac, sCenter, capital, alki);
 
 
-pikePlace.render();
-seaTac.render();
-sCenter.render();
-capital.render();
-alki.render();
+cookieStore.listHours();
 
+
+
+
+
+
+    // this.totalCookiesPerDay = 0;
+    // this.totalCookiesPerHour = [];
+    // console.log(this.totalCookiesPerDay, this.totalCookiesPerHour);
+//
+//
+// //generate number of hourly customers per store
+// Store.prototype.generateRandom = function () {
+//   return Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+//   console.log('math');
+//
+//
+// //generate number of cookies per hour
+// Store.prototype.cookiesPerHour = function () {
+//   return Math.floor(this.generateRandom() * this.avgCookies);
+//   this.generateRandom = hourTotal ;
+// };
+// //generate number of total cooies per day
+// Store.prototype.cookiesPerDay = function () {
+// //    var cookiesDay;
+//   for (var i = 6; i < 20; i++) {
+//     var cookiesHere = this.cookiesPerHour();
+//     this.hourCookies.push(cookiesHere);
+//     this.totalCookies += cookiesHere;
+//   }
+// };
+//var pikePlace = new Store('Pike Place', 6.3, 23, 65);
+// var seaTac = new Store('SeaTac Airport', 1.2, 3 , 24);
+// var sCenter = new Store('Seattle Center', 3.7, 11, 28);
+// var capital = new Store('Capital Hill', 2.3, 20, 38);
+// var alki = new Store('Alki', 4.6, 2, 16);
